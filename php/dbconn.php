1612e409e7cb6818305697ac39dc60bb00f15291
Tomasz Mizak <tomasz.mizak99@gmail.com>
2021-02-26 23:09:28 +0100

    upd

--- php/dbconn.php
+++ php/dbconn.php
@@ -3,5 +3,5 @@
     require_once "config.php";
 
     $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME) or die("Wystąpił błąd, prosimy o przekazanie informacji na adres tonasz.mizak@wpia.uni.lodz.pl");
-
+    mysqli_set_charset($link, "utf8");
 ?>
